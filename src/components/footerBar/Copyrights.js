aa56e3bae62ec7a9af027d2ce0b54c26b7eb8eb0
Ewelina <ewelina@ekopacz.pl>
2022-02-22 22:22:32 +0100

    added styles

--- src/components/footerBar/Copyrights.js
+++ src/components/footerBar/Copyrights.js
@@ -6,6 +6,7 @@ const copyrightsStyle = {
     letterSpacing:'1px',
     fontSize:'16px',
     paddingLeft:'30px',
+    paddingRight:'30px',
     boxShadow: 'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px',
 }
 
